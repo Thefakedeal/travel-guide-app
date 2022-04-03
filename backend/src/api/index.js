6c67007a13fe24b8bc20be09cc8476f86feae56c
Samir Shrestha <theonlysamir@gmail.com>
2022-04-03 13:21:13 +0545

    Boiler plate

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,12 +1,5 @@
-const express = require('express');
+const router = require('express').Router();
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏'
-  });
-});
 
 
 
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,29 +1,12 @@
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-
-require('dotenv').config();
-
-const middlewares = require('./middlewares');
-const api = require('./api');
-
+const api = require('./api/index.js');
 const app = express();
 
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
-  });
+app.get('/', (req, res)=>{
+    res.json({message: "Hello World"});
 });
 
-app.use('/api/v1', api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use('/api',api);
 
 module.exports = app;
