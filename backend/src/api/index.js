5538f9d19e522f42f217d254fe28a959a080cdd7
Samir Shrestha <theonlysamir@gmail.com>
2022-04-06 15:39:31 +0545

    Experience added

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const auth = require('./auth')
 const cities = require('./cities') 
 const places = require('./places')
+const experiences = require('./experiences')
 
 
 router.use('/auth', auth);
 router.use('/cities', cities);
 router.use('/places', places);
+router.use('/experiences', experiences);
 
 module.exports = router;
