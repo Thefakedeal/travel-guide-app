46fa0715016c8818e513960bab4d176cb4ba69ff
Samir Shrestha <theonlysamir@gmail.com>
2022-04-06 13:26:30 +0545

    City module added

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const auth = require('./auth')
+const cities = require('./cities') 
+
+
 
 router.use('/auth', auth);
+router.use('/cities', cities);
 
 module.exports = router;
