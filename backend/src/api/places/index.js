54ed3f528a27b3ed43c4f4a9fdd88809ea9a67df
Samir Shrestha <theonlysamir@gmail.com>
2022-04-07 22:22:27 +0545

    Image can be deleted

--- backend/src/api/places/index.js
+++ backend/src/api/places/index.js
@@ -1,87 +1,100 @@
-const router  = require('express').Router();
-const {body, check,validationResult} = require('express-validator')
-const db = require('../../client')
-const upload = require('../../utils/multer')
-const nameRequired = body('name').notEmpty().trim();
-
-const cityRequired = body('cityId').notEmpty().custom(async (value) =>{
-    if(!value)  return Promise.reject("City Doesn't exist");
+const router = require("express").Router();
+const { body, check, validationResult } = require("express-validator");
+const db = require("../../client");
+const upload = require("../../utils/multer");
+const nameRequired = body("name").notEmpty().trim();
+const { unlink } = require("fs");
+const path = require("path");
+const cityRequired = body("cityId")
+  .notEmpty()
+  .custom(async (value) => {
+    if (!value) return Promise.reject("City Doesn't exist");
     const city = await db.city.findUnique({
+      where: {
+        id: value,
+      },
+    });
+    if (!city) {
+      return Promise.reject("City Doesn't exist");
+    }
+    return true;
+  });
+
+const experiencesExists = body("experiences")
+  .isArray()
+  .custom(async (value) => {
+    if (!Array.isArray(value))
+      return Promise.reject("Experience Doesn't exist");
+
+    for (const item of value) {
+      const experience = await db.experience.findUnique({
         where: {
-          id: value,
+          id: item,
         },
       });
-      if (!city) {
-        return Promise.reject("City Doesn't exist");
+      if (!experience) {
+        return Promise.reject("Experience Doesn't exist");
       }
-      return true;
-})
-
-const experiencesExists = body('experiences').isArray().custom(async (value)=>{
-    if(!Array.isArray(value))  return Promise.reject("Experience Doesn't exist");
-    
-      for (const item of value) {
-        const experience = await db.experience.findUnique({
-            where: {
-              id: item,
-            },
-          });
-          if (!experience) {
-            return Promise.reject("Experience Doesn't exist");
-          }
-      }
-         
-      return true;
-})
-
-const featured = body('featured').isBoolean().optional({nullable: true})
-
-router.get('/', async (req,res,next)=>{
-    try{
-        const experienceFilter={};
-        if(req.query.experienceId != null && req.query.experienceId != null){
-            experienceFilter.some = {
-                id: (req.query.experienceId != null && req.query.experienceId != null) ? Number(req.query.experienceId): undefined
-            }
-        }
-        const places = await db.place.findMany({
-            where: {
-                cityId: req.query.cityId != null ?Number(req.query.cityId):undefined,
-                experience:experienceFilter
-            },
-            include:{
-                experience: true
-            } 
-        })
-        return res.json({"data": places})
-    }catch(err){
-        next(err)
     }
-})
 
-router.get('/:id', async (req,res,next)=>{
-    try{
-        const place = await db.place.findUnique({
-            where: {
-                id: Number(req.params.id)
-            },
-            include:{
-                experience: true, 
-                image: true
-            } 
-        });
-        if(!place) {
-            res.status(404)
-            next(new Error("Place not found"))
-        }
-        return res.json({data: place})
-    }catch(err){
-        next(err)
+    return true;
+  });
+
+const featured = body("featured").isBoolean().optional({ nullable: true });
+
+router.get("/", async (req, res, next) => {
+  try {
+    const experienceFilter = {};
+    if (req.query.experienceId != null && req.query.experienceId != null) {
+      experienceFilter.some = {
+        id:
+          req.query.experienceId != null && req.query.experienceId != null
+            ? Number(req.query.experienceId)
+            : undefined,
+      };
     }
-})
+    const places = await db.place.findMany({
+      where: {
+        cityId: req.query.cityId != null ? Number(req.query.cityId) : undefined,
+        experience: experienceFilter,
+      },
+      include: {
+        experience: true,
+      },
+    });
+    return res.json({ data: places });
+  } catch (err) {
+    next(err);
+  }
+});
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const place = await db.place.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+      include: {
+        experience: true,
+        image: true,
+      },
+    });
+    if (!place) {
+      res.status(404);
+      next(new Error("Place not found"));
+    }
+    return res.json({ data: place });
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post('/',nameRequired , cityRequired,featured, async (req,res, next)=>{
+router.post(
+  "/",
+  nameRequired,
+  cityRequired,
+  featured,
+  async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res
@@ -89,24 +102,28 @@ router.post('/',nameRequired , cityRequired,featured, async (req,res, next)=>{
         .json({ message: "Validation Error", errors: errors.array() });
     }
 
-    try{
-        const place = await db.place.create({
-            data:{
-                name: req.body.name,
-                cityId: req.body.cityId,
-                featured: req.body.featured,
-            }
-        })
+    try {
+      const place = await db.place.create({
+        data: {
+          name: req.body.name,
+          cityId: req.body.cityId,
+          featured: req.body.featured,
+        },
+      });
 
-        return res.json({data: place, message: "Place Created"})
-
-    }catch(err){
-        next(err)
+      return res.json({ data: place, message: "Place Created" });
+    } catch (err) {
+      next(err);
     }
-})
+  }
+);
 
-
-router.put('/:id',nameRequired,cityRequired, featured,async (req,res, next)=>{
+router.put(
+  "/:id",
+  nameRequired,
+  cityRequired,
+  featured,
+  async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res
@@ -114,116 +131,112 @@ router.put('/:id',nameRequired,cityRequired, featured,async (req,res, next)=>{
         .json({ message: "Validation Error", errors: errors.array() });
     }
 
-    try{
-        const place = await db.place.update({
-            where:{
-                id: Number(req.params.id)
-            },
-            data:{
-                name: req.body.name,
-                cityId: req.body.cityId,
-                featured: req.body.featured,
-            }
-        })
+    try {
+      const place = await db.place.update({
+        where: {
+          id: Number(req.params.id),
+        },
+        data: {
+          name: req.body.name,
+          cityId: req.body.cityId,
+          featured: req.body.featured,
+        },
+      });
 
-        return res.json({data: place, message: "Place Updated"})
-
-    }catch(err){
-        next(err)
+      return res.json({ data: place, message: "Place Updated" });
+    } catch (err) {
+      next(err);
     }
-})
+  }
+);
 
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const place = await db.place.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
 
-router.delete('/:id', async (req,res, next)=>{
-    try{
-        const place = await db.place.delete({
-            where:{
-                id: Number(req.params.id)
-            },
-        })
+    return res.json({ data: place, message: "Place deleted" });
+  } catch (err) {
+    next(err);
+  }
+});
 
-        return res.json({data: place, message: "Place deleted"})
-
-    }catch(err){
-        next(err)
+router.post("/:id/experiences", experiencesExists, async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ message: "Validation Error", errors: errors.array() });
     }
-})
 
-router.post('/:id/experiences', experiencesExists, async(req, res, next)=>{
-    try{
+    const { experiences } = req.body;
 
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res
-            .status(400)
-            .json({ message: "Validation Error", errors: errors.array() });
-        }
-        
-        const { experiences } = req.body;
+    const experiencesObj = experiences.map((experience) => {
+      return {
+        id: Number(experience),
+      };
+    });
 
-        const experiencesObj = experiences.map(experience=>{
-            return {
-                id: Number(experience)
-            }
-        })
-       
-        const place = await db.place.update({
-            where:{
-                id: Number(req.params.id)
-            },
-            data:{
-                experience:{
-                    connect: experiencesObj
-                }
-            },
-            include:{
-                experience: true
-            }
-        })
+    const place = await db.place.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        experience: {
+          connect: experiencesObj,
+        },
+      },
+      include: {
+        experience: true,
+      },
+    });
 
-        res.json({message: "Experiences connected", data: place})
-    }catch(err){
-        next(err)
+    res.json({ message: "Experiences connected", data: place });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id/experiences", experiencesExists, async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ message: "Validation Error", errors: errors.array() });
     }
-})
 
-router.delete('/:id/experiences', experiencesExists,async(req, res, next)=>{
-    try{
+    const { experiences } = req.body;
 
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res
-            .status(400)
-            .json({ message: "Validation Error", errors: errors.array() });
-        }
+    const experiencesObj = experiences.map((experience) => {
+      return {
+        id: Number(experience),
+      };
+    });
 
-        const { experiences } = req.body;
+    const place = await db.place.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        experience: {
+          disconnect: experiencesObj,
+        },
+      },
+      include: {
+        experience: true,
+      },
+    });
 
-        const experiencesObj = experiences.map(experience=>{
-            return {
-                id: Number(experience)
-            }
-        })
-
-        const place = await db.place.update({
-            where:{
-                id: Number(req.params.id)
-            },
-            data:{
-                experience:{
-                    disconnect: experiencesObj
-                }
-            },
-            include:{
-                experience: true
-            }
-        })
-
-        res.json({message: "Experiences removed", data: place})
-    }catch(err){
-        next(err)
-    }
-})
+    res.json({ message: "Experiences removed", data: place });
+  } catch (err) {
+    next(err);
+  }
+});
 
 const photoUpload = upload.array("images", 10);
 
@@ -234,41 +247,61 @@ const filesRequired = check("images")
   })
   .withMessage("Images are required");
 
-router.post('/:id/images',photoUpload, filesRequired, async(req,res, next)=>{
-    try{
-        
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res
-            .status(400)
-            .json({ message: "Validation Error", errors: errors.array() });
-        }
+router.post(
+  "/:id/images",
+  photoUpload,
+  filesRequired,
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res
+          .status(400)
+          .json({ message: "Validation Error", errors: errors.array() });
+      }
 
-        const uploadedImages = req.files;
+      const uploadedImages = req.files;
 
-        const place = await db.place.update({
-            where:{
-                id:  Number(req.params.id)
-            },
-            data:{
-                image: {
-                    create: uploadedImages.map(img=>{
-                        return {
-                            src: `/images/${img.filename}`,
-                        }
-                    })
-                }
-            }, 
-            include:{
-                image: true
-            }
-        })
+      const place = await db.place.update({
+        where: {
+          id: Number(req.params.id),
+        },
+        data: {
+          image: {
+            create: uploadedImages.map((img) => {
+              return {
+                src: `/images/${img.filename}`,
+              };
+            }),
+          },
+        },
+        include: {
+          image: true,
+        },
+      });
 
-        return res.json({message:"Photos Uploaded", data: place})
-    }catch(err){
-        next(err)
+      return res.json({ message: "Photos Uploaded", data: place });
+    } catch (err) {
+      next(err);
     }
-})
+  }
+);
 
+router.delete("/images/:id", async (req, res, next) => {
+  try {
+    const image = await db.image.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+
+    unlink(path.join("static", image.src), (err) => {
+      if (err) return next(err);
+      return res.json({ message: "Image Deleted", data: image });
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
